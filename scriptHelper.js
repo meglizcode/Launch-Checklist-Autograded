974bc62dba5ccfb373997bbd6a8ada3122265480
meglizcode <meglizjudd@gmail.com>
2024-05-08 12:16:51 -0500

    6 tests cleaner

--- scriptHelper.js
+++ scriptHelper.js
@@ -55,30 +55,29 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
 // Display Conformations //
 
-if (fuelLevel >= 10000 && cargoLevel <= 10000) {
-    launchStatus.style.color = 'green'
-    launchStatus.innerHTML = 'Shuttle is Ready for Launch'
-} else {
-    launchStatus.style.color = 'red'
-    launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
-}
+// Header //
+    if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+        launchStatus.style.color = 'green'
+        launchStatus.innerHTML = 'Shuttle is Ready for Launch'
+    } else {
+        launchStatus.style.color = 'red'
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
+    }
 
+// Fuel //
     if (fuelLevel >= 10000) {
          fuelStatus.style.color = 'green'
          fuelStatus.innerHTML = `Fuel level high enough for launch`
-        }
-
-    if (cargoLevel <= 10000) {
-         cargoStatus.style.color = 'green'
-         cargoStatus.innerHTML = `Cargo mass low enough for launch`
-    }
-
-    if (fuelLevel < 10000) {
+        } else {
         fuelStatus.style.color = 'red'
         fuelStatus.innerHTML = `Fuel level too low for launch`
     }
 
-    if (cargoLevel > 10000) {
+// Cargo //
+    if (cargoLevel <= 10000) {
+         cargoStatus.style.color = 'green'
+         cargoStatus.innerHTML = `Cargo mass low enough for launch`
+    } else {
         cargoStatus.style.color = 'red'
         cargoStatus.innerHTML = `Cargo mass too heavy for launch`
     }
